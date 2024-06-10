f76ffdfffb32c7192a8d2ea5310ad0e39cec8cdf
conghieu26 <hieunc00@gmail.com>
2024-06-10 21:46:13 +0700

    Create class Todo

---
+++ js/todo.js
@@ -0,0 +1,34 @@
+let id = 1;
+
+let STATUS = { 
+  DONE: "done",
+  IN_PROGRESS : "in-progress",
+}
+
+class Todo { 
+  constructor(name) { 
+    this.name = name; 
+  
+    this.__id = id; 
+
+    id += 1;
+
+    this.status =  STATUS.IN_PROGRESS;
+  }
+
+  get id() { 
+    return this.__id;
+  }
+
+  toggleTodoStatus() {
+    return this.status = this.isInProgress ? STATUS.DONE : STATUS.IN_PROGRESS;
+  }
+
+  isDone() { 
+    return this.status == STATUS.DONE;
+  }
+
+  isInProgress() {
+    return this.status == STATUS.IN_PROGRESS;
+  }
+}
